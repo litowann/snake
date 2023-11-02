b9cd54ef5ec46723e778fbaacfc6862adaa26cf3
Oleh Khmelevskyi <133887523+litowann@users.noreply.github.com>
2023-11-02 10:39:04 +0200

    update snake game over

--- snake.py
+++ snake.py
@@ -12,11 +12,7 @@ class Snake:
 
     def set_position(self):
         for position in STARTING_POSITIONS:
-            snake_segment = Turtle("square")
-            snake_segment.color("white")
-            snake_segment.penup()
-            snake_segment.goto(position)
-            self.segments.append(snake_segment)
+            self.add_segment(position)
 
     def move(self):
         for segment_index in range(len(self.segments) - 1, 0, -1):
@@ -25,6 +21,16 @@ class Snake:
             self.segments[segment_index].goto(x, y)
 
         self.head.forward(MOVE_DISTANCE)
+
+    def add_segment(self, position):
+        snake_segment = Turtle("square")
+        snake_segment.color("white")
+        snake_segment.penup()
+        snake_segment.goto(position)
+        self.segments.append(snake_segment)
+
+    def extend(self):
+        self.add_segment(self.segments[-1].position())
 
     def up(self):
         if self.head.heading() != DOWN_DIRECTION:
